6e9ff52458a4f339f321e6deea5d52afd7ee453c
Juan Manuel Giron <juan.giron@mi.unc.edu.ar>
2021-06-13 18:32:54 -0300

    removed trash files

--- src/Metodo_de_pago.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/** Esta interfaz es utilizada para implementar distintas funcionalidades en el metodo de pago
- * @author: Pineapple
- * @version: 12/06/2021
- */
-public interface Metodo_de_pago {
-    public boolean pagar(int monto);
-}
